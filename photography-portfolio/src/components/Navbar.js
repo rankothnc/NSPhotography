e12309581026dea8933b78070378b7ea15f263ec
rankothnc <ncrankoth@gmail.com>
2024-09-02 11:35:54 +0530

    dropdown in photography navbar

--- photography-portfolio/src/components/Navbar.js
+++ photography-portfolio/src/components/Navbar.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/navbar.css';
 
+
 const Navbar = () => {
+
+const [dropdownOpen, setDropdownOpen] = useState(false);
+
+const toggleDropdown =()=>{
+    setDropdownOpen(!dropdownOpen);
+};
+
     return (
         <nav className='navbar'>
             <div className='logo'>
@@ -11,7 +19,17 @@ const Navbar = () => {
             </div>
             <ul className='nav-links'>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/photography">Photography</Link></li>
+                <li className='dropdown' onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
+                    <span>Photography</span>
+                    {dropdownOpen && (
+                        <ul className='dropdown-menu'>
+                            <li><Link to="/wildlife">Wildlife</Link> </li>
+                            <li><Link to="/location">Location</Link> </li>
+                            <li><Link to="/landscape">Landscape</Link> </li>
+                            <li><Link to="/travel">Travel</Link> </li>
+                        </ul>
+                    )}
+                </li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
